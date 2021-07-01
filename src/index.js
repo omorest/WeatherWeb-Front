78110436b0544d209c893d5e40ea79b09bfb281c
omorest <oscar.moreira.estevez@gmail.com>
2021-07-01 20:42:47 +0100

    creating html weather in js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import {getDataWeatherByLatLon} from './api/requests'
 import './style.css';
 
-const cityCountry = document.querySelector('.city-country')
-const tempIcon = document.querySelector('.temp-icon')
-const weatherIconContainer = document.querySelector('.weather')
-const descriptionWeather = document.querySelector('.description')
+const weatherInfo = document.querySelector('.weather-info')
 
 const iniInformationWeather = () => {
 	async function success(position) {
@@ -16,28 +13,27 @@ const iniInformationWeather = () => {
 }
 
 const createInformationWeather = (weather) => {
-	let city = document.createElement('h2');
-	let country = document.createElement('h3');
-	let temperature = document.createElement('span');
-	let iconTemperature = document.createElement('img')
-	let icon = document.createElement('img')
-	let description = document.createElement('h3')
-	
-	temperature.classList.add('temperature')
-	city.innerHTML = weather.city;
-	country.innerHTML = weather.country;
-	cityCountry.append(country, city);
-	
-	temperature.innerHTML = weather.temperature;
-	iconTemperature.src = './images/temperature.png';
-	tempIcon.append(temperature, iconTemperature);
+	const html = `
+	<div class="city-country">
+		<h3>${weather.country}</h3>
+		<h2>${weather.city}</h2>
+	</div>
+	<div class="temp-icon-desc">
+		<div class="temp-icon">
+			<span class="temperature">${weather.temperature}</span>
+			<img src="./images/temperature.png">
+		</div>
+		<div class="weather">
+			<img src=\"./images/${weather.icon}.png\">
+		</div>
+		<div class="description">
+			<h3>${weather.description}</h3>
+		</div>
+	</div>
+	<div class="extra-info"></div>
+	`
 
-	icon.src = './images/' + weather.icon + '.png';
-	weatherIconContainer.append(icon);
-	
-	description.innerHTML = weather.description;
-	descriptionWeather.append(description)
+	weatherInfo.innerHTML = html;
 }
 
 iniInformationWeather()
-
