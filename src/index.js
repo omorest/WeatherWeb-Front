f4f3738c05f9ecc67c69d75a1550111a17f8ecc2
omorest <oscar.moreira.estevez@gmail.com>
2021-07-03 23:58:44 +0100

    size font

--- src/index.js
+++ src/index.js
@@ -15,33 +15,33 @@ const iniInformationWeather = () => {
 
 const createInformationWeather = (weather) => {
 	const html = `
-	<div class="city-country">
-		<h3>${weather.country}</h3>
-		<h2>${weather.city}</h2>
-	</div>
-	<div class="temp-icon-desc">
-		<div class="temp-icon">
-			<span class="temperature">${weather.temperature}</span>
-			<img src="./images/temperature.png">
+		<div class="city-country">
+			<h3>${weather.country}</h3>
+			<h2>${weather.city}</h2>
 		</div>
-		<div class="weather">
-			<img src=\"./images/${weather.icon}.png\">
+		<div class="temp-icon-desc">
+			<div class="temp-icon">
+				<span class="temperature">${weather.temperature}</span>
+				<img src="./images/temperature.png">
+			</div>
+			<div class="weather">
+				<img src=\"./images/${weather.icon}.png\">
+			</div>
+			<div class="description">
+				<h3>${weather.description}</h3>
+			</div>
 		</div>
-		<div class="description">
-			<h3>${weather.description}</h3>
+		<div class="extra-info">
+			<div class="humidity">
+				<img src="./src/images/humidity.png" alt="humidity percent">
+				<p><strong>${weather.humidity} %</strong></p>
+			</div>
+			<div class="wind">
+				<img src="./src/images/wind.png" alt="wind speed">
+				<p><strong>${weather.speedWind} km/h</strong></p>
+			</div>
 		</div>
-	</div>
-	<div class="extra-info">
-		<div class="humidity">
-			<img src="./src/images/humidity.png" alt="humidity percent">
-      <p><strong>${weather.humidity} %</strong></p>
-		</div>
-		<div class="wind">
-			<img src="./src/images/wind.png" alt="wind speed">
-      <p><strong>${weather.speedWind} km/h</strong></p>
-		</div>
-	</div>
-	`
+		`
 
 	weatherInfo.innerHTML = html;
 }
