bd4ac22a774632cec0dcded4de3e2b05a8e4ef4f
omorest <oscar.moreira.estevez@gmail.com>
2021-07-07 21:15:38 +0100

    redesing

--- src/index.js
+++ src/index.js
@@ -16,32 +16,64 @@ const iniInformationWeather = () => {
 const createInformationWeather = (weather) => {
 	const html = `
 		<div class="city-country">
-			<h3>${weather.country}</h3>
-			<h2>${weather.city}</h2>
+			<span class="city">${weather.city}</span>
+			<span class="country">${weather.country}</span>
 		</div>
-		<div class="temp-icon-desc">
-			<div class="temp-icon">
-				<span class="temperature">${weather.temperature}</span>
-				<img src="./images/temperature.png">
-			</div>
-			<div class="weather">
+		<div class="temperature-info">
+			<span class="temperature">${weather.temperature}°C</span>
+		</div>
+		
+		<div class="squares-info">
+		<div class="square humidity">
+			<span><strong>Humidity</strong></span>
+			<img src="./src/images/humidity.png" alt="humidity percent">
+			<span><strong>${weather.humidity} %</strong></span>
+			</div>	
+			<div class="square weather">
 				<img src=\"./images/${weather.icon}.png\">
+				<span><strong>${weather.description}</strong></span>
 			</div>
-			<div class="description">
-				<h3>${weather.description}</h3>
-			</div>
+		<div class="square wind">
+			<span><strong>Wind Speed</strong></span>
+			<img src="./src/images/wind.png" alt="wind speed">
+			<span><strong>${weather.speedWind} Km/h</strong></span>
 		</div>
-		<div class="extra-info">
-			<div class="humidity">
-				<img src="./src/images/humidity.png" alt="humidity percent">
-				<p><strong>${weather.humidity} %</strong></p>
-			</div>
-			<div class="wind">
-				<img src="./src/images/wind.png" alt="wind speed">
-				<p><strong>${weather.speedWind} km/h</strong></p>
-			</div>
 		</div>
+
+		
 		`
+		// 	<div class="humidity">
+		// 		<img src="./src/images/humidity.png" alt="humidity percent">
+		// 		<p><strong>${weather.humidity} %</strong></p>
+		// 	</div>
+		// 	<div class="wind">
+		// 		<img src="./src/images/wind.png" alt="wind speed">
+		// 		<p><strong>${weather.speedWind} km/h</strong></p>
+		// 	</div>
+		//`
+
+	// const html = `
+	// 	<div class="city-country">
+	// 		<h3>${weather.country}</h3>
+	// 		<h2>${weather.city}</h2>
+	// 	</div>
+	// 	<div class="temp-icon-desc">
+	// 		<div class="temp-icon">
+	// 			<span class="temperature">${weather.temperature}</span>
+	// 		</div>
+	// 		<div class="weather">
+	// 			<img src=\"./images/${weather.icon}.png\">
+	// 			<h3>${weather.description}</h3>
+	// 	</div>
+	// 		<div class="humidity">
+	// 			<img src="./src/images/humidity.png" alt="humidity percent">
+	// 			<p><strong>${weather.humidity} %</strong></p>
+	// 		</div>
+	// 		<div class="wind">
+	// 			<img src="./src/images/wind.png" alt="wind speed">
+	// 			<p><strong>${weather.speedWind} km/h</strong></p>
+	// 		</div>
+	// 	`
 
 	weatherInfo.innerHTML = html;
 }
